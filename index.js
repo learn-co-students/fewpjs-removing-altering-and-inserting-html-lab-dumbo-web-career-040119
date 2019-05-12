e56379db4103b3c165b2bdcc11a741be2d626026
Susan Tan <ssusantan9@gmail.com>
2019-05-12 19:36:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+let main = document.querySelector("main").remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = "victory";
+// newHeader.innerText = "Susantano is the champion";
+newHeader.innerHTML = "Susantano oreo cupcake pineapples is the champion";
+
+let appendNewHeader = document.body.appendChild(newHeader);
